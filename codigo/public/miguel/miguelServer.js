787cf576b7cb4b995ae0defb30ddecbb691e1bc2
miguel <miguelteixeiragk12@gmail.com>
2025-05-18 02:22:19 -0300

    correcao de codigos

--- codigo/public/miguel/miguelServer.js
+++ codigo/public/miguel/miguelServer.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-let comentarios = [];
+const DB_PATH = './miguel.json';
+
+
+function lerComentarios() {
+  if (!fs.existsSync(DB_PATH)) {
+    fs.writeFileSync(DB_PATH, JSON.stringify({ comentarios: [] }, null, 2));
+  }
+  const data = fs.readFileSync(DB_PATH);
+  return JSON.parse(data).comentarios;
+}
+
+
+function salvarComentarios(comentarios) {
+  fs.writeFileSync(DB_PATH, JSON.stringify({ comentarios }, null, 2));
+}
 
 app.get('/comentarios', (req, res) => {
+  const comentarios = lerComentarios();
   res.json(comentarios);
 });
 
 app.post('/comentarios', (req, res) => {
+  const comentarios = lerComentarios();
   const novoComentario = req.body;
   comentarios.push(novoComentario);
+  salvarComentarios(comentarios);
   res.json(novoComentario);
 });
 
